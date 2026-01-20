51f74d7449d98916a36fea4b9a2c123647b88e78
liunan <boy_liunan@hotmail.com>
2026-01-20 14:41:51 +0800

    fix

--- install/entrypoint.sh
+++ install/entrypoint.sh
@@ -9,19 +9,8 @@ function create_db {
     echo "Initializing DM database..."
     rm -rf /dbdata/*
     $DM_HOME/dminit path=$DATA_DIR charset=${CHARSET:-1} page_size=${PAGE_SIZE:-32} \
-    case_sensitive=${CASE_SENSITIVE:-0} ARCH_FLAG=${ARCH_FLAG:-1} PORT_NUM=${PORT_NUM:-5236} \
+    case_sensitive=${CASE_SENSITIVE:-0} PORT_NUM=${PORT_NUM:-5236} \
     SYSDBA_PWD=${SYSDBA_PWD:-Dameng123} SYSAUDITOR_PWD=${SYSAUDITOR_PWD:-Dameng123}
-
-    if [ "$ARCH_FLAG" = "1" ]; then
-        cat > $DATA_DIR/DAMENG/dmarch.ini << EOF
-[ARCHIVE_LOCAL1]
-ARCH_TYPE = LOCAL
-ARCH_DEST = /dbarch
-ARCH_FILE_SIZE = 256
-ARCH_SPACE_LIMIT = 319600
-ARCH_FLUSH_BUF_SIZE = 0
-EOF
-    fi
 }
 
 function start_db {
